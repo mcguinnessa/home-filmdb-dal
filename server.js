1c239689323421930d980991e5449d870c3a5e51
Alex McGuinness <github@alexmcguinness.me.uk>
2023-07-20 17:21:13 +0100

    add env vars

--- server.js
+++ server.js
@@ -1,17 +1,31 @@
 const express = require('express');
 const mongoose = require('mongoose');
 
+const mongodb_user = process.env.MONGODB_USER;
+const mongodb_password = process.env.MONGODB_PASSWORD;
+const mongodb_uri = process.env.MONGODB_URI;
+const mongodb_doc = process.env.MONGODB_DOC;
+
+console.log("URI:" + mongodb_uri);
+console.log("DB:" + mongodb_doc);
+console.log("user:" + mongodb_user);
+console.log("pass:" + mongodb_password);
 
 
-const mongo_str = "mongodb://192.168.0.116:30164/FilmDatabase"
+const mongo_str = "mongodb://"+mongodb_uri+"/" + mongodb_doc
+//const mongo_str = "mongodb://192.168.0.116:30164/FilmDabase";
 //const mongo_str = "mongodb://film_mgr:paramount@192.168.0.116:30164/FilmDatabase"
 //const mongo_str = "mongodb://film_mgr:paramount@192.168.0.116:30164"
 
 
+console.log("Connecting:" + mongo_str)
+
 mongoose.connect(mongo_str, {
   authSource: "admin",
-  user: "film_mgr",
-  pass: "paramount",
+//  user: "film_mgr",
+//  pass: "paramount",
+  user: mongodb_user,
+  pass: mongodb_password,
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
@@ -23,7 +37,7 @@ db.on('error', (error) => {
 })
 
 db.once('connected', () => {
-    console.log('Database Connected');
+    console.log('Database Connected: ' + mongo_str);
 })
 
 
